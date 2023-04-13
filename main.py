3b9f0568c8a255678724740571edacb72a03ee93
brrruski <sizhe4real@gmail.com>
2023-04-13 15:51:19 -0400

    feat: get phone_ip and cloud_ip from traces

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import datetime
 from collections import deque
 from typing import List, Dict, TextIO
 
+import pyshark
+
 from process_app_log import Phase, get_phases_from_lines, is_formal_log, has_prefix, extract_timestamp, generate_moment, \
     extract_stroke_id
+from process_pcap import get_ip
 
 OUTPUT_DIR = 'output'
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
 
 def input_path(file_path: str) -> str:
@@ -20,7 +24,7 @@ def output_path(file_path: str) -> str:
 
 
 # accecpt a file path, open it and apply the get_phases_from_lines function, and output the list of phases
-def process_app_log(file_path: str, type: str, metadata: Dict = None) -> Dict[str, Phase]:
+def process_app_log(file_path: str, type: str, metadata: Dict = None) -> List[Phase]:
     metadata = metadata if metadata is not None else {}
     with open(file_path, 'r') as f:
         if type == 'host':
@@ -30,12 +34,10 @@ def process_app_log(file_path: str, type: str, metadata: Dict = None) -> Dict[st
         return []
 
 
-def process_app_log_for_host(file: TextIO) -> Dict[str, Phase]:
+def process_app_log_for_host(file: TextIO) -> List[Phase]:
     phase_1a = Phase(phase='1a', desc='touch screen to first data pkt sent')
     task_queue = deque([phase_1a])
-    res_phases = {
-        '1a': phase_1a
-    }
+    res_phases = [phase_1a]
     lines = file.readlines()
     lines = filter(is_formal_log, lines)
     moments = list(map(generate_moment, lines))
@@ -64,14 +66,11 @@ def process_app_log_for_host(file: TextIO) -> Dict[str, Phase]:
     return res_phases
 
 
-def process_app_log_for_resolver(file: TextIO, stroke_id: str) -> Dict[str, Phase]:
+def process_app_log_for_resolver(file: TextIO, stroke_id: str) -> List[Phase]:
     phase_2a = Phase(phase='2a', desc='first data pkt from cloud to last pkt from cloud')
     phase_2d = Phase(phase='2d', desc='end rendered time to last pkt from cloud')
     task_queue = deque([phase_2a, phase_2d])
-    res_phases = {
-        '2a': phase_2a,
-        '2d': phase_2d,
-    }
+    res_phases = [phase_2a, phase_2d]
     lines = file.readlines()
     lines = filter(is_formal_log, lines)
     moments = list(map(generate_moment, lines))
@@ -117,16 +116,69 @@ def process_app_log_for_resolver(file: TextIO, stroke_id: str) -> Dict[str, Phas
 #     print("output file: {}".format(file_name))
 
 
-def merge_timeline(host_app_log, resolver_app_log):
+def process_pcap_trace(file_path: str, type: str, start_time: datetime, end_time: datetime) -> List[Phase]:
+    with open(file_path, 'r') as f:
+        if type == 'host':
+            return process_pcap_trace_for_host(file_path, start_time, end_time)
+        elif type == 'resolver':
+            return process_pcap_trace_for_resolver(file_path)
+        return []
+
+
+def process_pcap_trace_for_host(file_path: str, start_time: datetime, end_time: datetime) -> List[Phase]:
+    # filter timestamp by e2e start and end, and filter out the data pkt & ack pkt
+    # for the first data pkt, the source is phone, the destination is cloud
+    # collect the data pkt and its ack as a pair
+    #     if the source is phone and the content is Application Data, then it is 1b start
+    #     if the source is cloud and the content is ACK, then it is 1b end
+    #     if the source is cloud and the content is Application Data, then it is 1c start
+    #     if the source is phone and the content is ACK, then it is 1c end
+    # return all pairs
+    pyshark_filters = []
+    pyshark_filters.append('frame.time >= "{st}"'.format(st=start_time.strftime(DATETIME_FORMAT)))
+    pyshark_filters.append('frame.time <= "{et}"'.format(et=end_time.strftime(DATETIME_FORMAT)))
+    pyshark_filters.append('tcp')
+    cap_e2e = pyshark.FileCapture(file_path, display_filter='&&'.join(pyshark_filters))
+    ip_set = set()
+    phone_ip = None
+    cloud_ip = None
+    for pkt in cap_e2e:
+        ip_set.add(get_ip(pkt, type='src'))
+        ip_set.add(get_ip(pkt, type='dst'))
+
+        # assume the first is data pkt from phone and the second is ack pkt from cloud
+        if phone_ip is None:
+            phone_ip = get_ip(pkt, type='src')
+        elif phone_ip is not None and cloud_ip is None:
+            cloud_ip = get_ip(pkt, type='src')
+        pass
+    return []
+
+
+def process_pcap_trace_for_resolver(file_path: str) -> List[Phase]:
+    return []
+
+
+def merge_timeline(host_app_log, resolver_app_log, host_pcap, resolver_pcap):
     host_app_phases = process_app_log(input_path(host_app_log), type='host')
+    phase_1a = host_app_phases[0]
+
     resolver_app_phases = process_app_log(input_path(resolver_app_log), type='resolver', metadata={
-        "stroke_id": host_app_phases.get('1a').metadata.get("stroke_id")
+        "stroke_id": phase_1a.metadata.get("stroke_id")
     })
-    # host_pcap = process_app_log(input_path(host_pcap), ["1a start", "2a end - 2d start", "2d"])
+
+    phase_2d = resolver_app_phases[-1]
+    t1 = phase_1a.start_time
+    t7 = phase_2d.end_time
+
+    host_pcap = process_pcap_trace(
+        input_path(host_pcap),
+        type='host',
+        start_time=t1,
+        end_time=t7
+    )
     # resolver_pcap = process_app_log(input_path(resolver_pcap), ["1a start", "2a end - 2d start", "2d"])
 
-    t1 = host_app_phases.get('1a').start_time
-    t2 = datetime.datetime()
     t3 = datetime.datetime()
     t4 = datetime.datetime()
     t5 = datetime.datetime()
@@ -156,4 +208,9 @@ def diff_sec(t1, t2):
 
 
 if __name__ == '__main__':
-    merge_timeline('./0407/host/0407-run1/static_log.logcat', './0407/resolver/0407-run1/static_log.logcat')
+    merge_timeline(
+        host_app_log='./0407/host/0407-run1/static_log.logcat',
+        resolver_app_log='./0407/resolver/0407-run1/static_log.logcat',
+        host_pcap='./0407/host/0407-run1/capture.pcap',
+        resolver_pcap='./0407/resolver/0407-run1/capture.pcap',
+    )
